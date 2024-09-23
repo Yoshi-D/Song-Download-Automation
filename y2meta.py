92b5b90d1dcb2a5eed25f3cfee05285c50fb9c9a
Yoshi-D <156103060+Yoshi-D@users.noreply.github.com>
2024-09-23 19:58:30 +0530

    Update y2meta.py

--- y2meta.py
+++ y2meta.py
@@ -5,7 +5,7 @@ driver = webdriver.Safari()
 website = "https://flvto.com.co/en11/"
 driver.get(website)
 song_list = ["https://www.youtube.com/watch?v=ZcZvVCaoeWU&pp=ygUPa2l5YSBraXlhIGF1ZGlv",
-             "https://www.youtube.com/watch?v=9hTMZkzIv4w&pp=ygUTbmFyYXlhbiBuYXJjaSBhdWRpbw%3D%3D"]
+             "https://www.youtube.com/watch?v=9hTMZkzIv4w&pp=ygUTbmFyYXlhbiBuYXJjaSBhdWRpbw%3D%3D"] #edit this for different youtube songs 
 for i in song_list:
     search_box = driver.find_element("xpath", "//*[@id='input']")
     search_box.send_keys(i)
@@ -30,7 +30,7 @@ for i in song_list:
     driver.switch_to.window(driver.window_handles[-1])
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
-    time.sleep(15) # instead of waiting i can check if the file s downloaded
+    time.sleep(15) 
     home = driver.find_element("xpath", "//*[@id='buttons']/a[3]") # back home for the next song
     home.click()
 driver.close()
